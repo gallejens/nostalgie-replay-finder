655d754197c2b873a55578c942a5dc3c647611a9
gallejens <galle.jens@telenet.be>
2024-08-27 15:04:44 +0200

    feat: detecting new track playing

--- main.go
+++ main.go
@@ -4,22 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
-type TracksResponse struct {
+type APIResponse struct {
 	Data []Track `json:"data"`
 }
 
-type Track = struct {
-	ID                int    `json:"id"`
-	RadioID           int    `json:"radio_id"`
-	TrackID           string `json:"track_id"`
-	Title             string `json:"title"`
-	Artist            string `json:"artist"`
-	DurationInSeconds int    `json:"duration_in_seconds"`
-	PlayedAt          string `json:"played_at"`
+type Track struct {
+	ID       int    `json:"id"`
+	RadioID  int    `json:"radio_id"`
+	TrackID  string `json:"track_id"`
+	Title    string `json:"title"`
+	Artist   string `json:"artist"`
+	PlayedAt string `json:"played_at"`
 }
 
 func getCurrentTrack() (Track, error) {
@@ -34,7 +34,13 @@ func getCurrentTrack() (Track, error) {
 		return Track{}, err
 	}
 
-	trackResponse := TracksResponse{}
+	rateLimitRemaining, err := strconv.Atoi(resp.Header.Get("x-ratelimit-remaining"))
+	if err != nil || rateLimitRemaining < 200 {
+		fmt.Println("Rate limit has gone below 200")
+		return Track{}, err
+	}
+
+	trackResponse := APIResponse{}
 	err = json.Unmarshal(body, &trackResponse)
 	if err != nil {
 		return Track{}, err
@@ -43,11 +49,32 @@ func getCurrentTrack() (Track, error) {
 	return trackResponse.Data[0], nil
 }
 
-func main() {
-	currentTrack, err := getCurrentTrack()
-	if err != nil {
-		log.Fatal(err)
-	}
+func isSameTrack(first Track, second Track) bool {
+	return first.Artist == second.Artist && first.Title == second.Title
+}
 
-	fmt.Println(currentTrack.Title)
+func main() {
+	var previousTrack Track = Track{}
+
+	for {
+		currentTrack, err := getCurrentTrack()
+		if err != nil {
+			fmt.Println(err)
+			time.Sleep(30 * time.Second)
+			continue
+		}
+
+		var sleepDuration time.Duration = 10
+
+		if !isSameTrack(previousTrack, currentTrack) {
+			if previousTrack != (Track{}) {
+				sleepDuration = 90
+			}
+
+			fmt.Printf("Now playing: %s - %s\n", currentTrack.Title, currentTrack.Artist)
+		}
+
+		previousTrack = currentTrack
+		time.Sleep(sleepDuration * time.Second)
+	}
 }
