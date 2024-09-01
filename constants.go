001a4b2a6cb9f053606c7f8be99630985a4b4751
gallejens <galle.jens@telenet.be>
2024-09-01 14:18:26 +0200

    chore: extra constant

--- constants.go
+++ constants.go
@@ -1,4 +1,6 @@
 package main
 
-const POLLING_RATE = 2 // seconds
+const POLLING_RATE = 2               // seconds
+const POLLING_TIMEOUT_ON_MATCH = 120 // seconds, timeout for polling when a new track has been found
+
 const API_ENDPOINT = "https://mediahuis.pointbreak.dev/api/v1/radio/9/tracks"
--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func populateTrackList() {
 	slices.Reverse(tracks)
 	tracklist = tracks
 
-	fmt.Println("Tracklist has been populated with previous tracks. Starting application")
+	fmt.Println("Tracklist has been populated with previous tracks")
 }
 
 func startTrackPolling() {
@@ -36,7 +36,7 @@ func startTrackPolling() {
 		var sleepDuration time.Duration = POLLING_RATE
 
 		if !isSameTrack(tracklist[len(tracklist)-1], newTrack) {
-			sleepDuration = 90
+			sleepDuration = POLLING_TIMEOUT_ON_MATCH
 
 			alreadyPlayed := checkTrackAlreadyPlayed(newTrack)
 			logMessage := fmt.Sprintf("Now playing: %s - %s | Already played: %s", newTrack.Title, newTrack.Artist, strconv.FormatBool(alreadyPlayed))
@@ -65,9 +65,7 @@ func main() {
 	populateTrackList()
 
 	// then we start polling, to check new tracks
-	go func() {
-		startTrackPolling()
-	}()
+	go startTrackPolling()
 
 	// we start the web server
 	startServer()
